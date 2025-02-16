4c22df3d1168bbfc4f1123ca5f168b491f09e3dc
Prashwarmishra <prashwarmishra@gmail.com>
2025-02-16 15:53:55 +0530

    implement GetPaymentIntent handler

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -3,30 +3,63 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	cards "github.com/go-stripe/internal/card"
 )
 
 type stripePayload struct {
-	Amount   int    `json:"amount"`
+	Amount   string `json:"amount"`
 	Currency string `json:"currency"`
 }
 
 type jsonResponse struct {
 	OK      bool   `json:"ok"`
-	Message string `json:"message"`
-	Content string `json:"content"`
-	ID      int    `json:"id"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      int    `json:"id,omitempty"`
 }
 
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	res := jsonResponse{
-		OK: true,
+	stripePayload := stripePayload{}
+
+	err := json.NewDecoder(r.Body).Decode(&stripePayload)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return
 	}
 
-	json, err := json.MarshalIndent(res, "", "    ")
+	amount, err := strconv.Atoi(stripePayload.Amount)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	card := cards.Card{
+		Key:      app.config.stripe.key,
+		Secret:   app.config.stripe.secret,
+		Currency: stripePayload.Currency,
+	}
+
+	pi, errMsg, err := card.ChargeCard(card.Currency, amount)
+
+	var res any
+
+	if err != nil {
+		res = jsonResponse{
+			OK:      false,
+			Message: errMsg,
+		}
+	} else {
+		res = pi
+	}
+
+	json, err := json.MarshalIndent(res, "", "   ")
 
 	if err != nil {
 		app.errorLog.Println("error in marshalling response", err)
-		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
