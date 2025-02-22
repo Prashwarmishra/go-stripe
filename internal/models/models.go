f98b168be61d2e73de0be8f41c40c165ba3d0176
Prashwarmishra <prashwarmishra@gmail.com>
2025-02-22 15:06:09 +0530

    create database models

--- internal/models/models.go
+++ internal/models/models.go
@@ -17,11 +17,57 @@ type Models struct {
 type Widget struct {
 	ID             int    `json:"id"`
 	Name           string `json:"name"`
+	Image          string `json:"image"`
 	Description    string `json:"description"`
 	Price          int    `json:"price"`
 	InventoryLevel int    `json:"inventory_level"`
 	CreatedAt      string `json:"-"`
 	UpdatedAt      string `json:"-"`
+}
+
+type Orders struct {
+	ID            int    `json:"id"`
+	WidgetId      int    `json:"widget_id"`
+	TransactionId int    `json:"transaction_id"`
+	StatusId      int    `json:"status_id"`
+	Quantity      int    `json:"quantity"`
+	CreatedAt     string `json:"-"`
+	UpdatedAt     string `json:"-"`
+}
+
+type Status struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	CreatedAt string `json:"-"`
+	UpdatedAt string `json:"-"`
+}
+
+type TransactionStatus struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	CreatedAt string `json:"-"`
+	UpdatedAt string `json:"-"`
+}
+
+type Transaction struct {
+	ID                  int    `json:"id"`
+	Amount              int    `json:"amount"`
+	Currency            string `json:"currency"`
+	LastFour            string `json:"last_four"`
+	BankReturnCode      string `json:"bank_return_code"`
+	TransactionStatusId int    `json:"transaction_status_id"`
+	CreatedAt           string `json:"-"`
+	UpdatedAt           string `json:"-"`
+}
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"-"`
+	UpdatedAt string `json:"-"`
 }
 
 func (m *DBModel) GetWidget(id int) (Widget, error) {
